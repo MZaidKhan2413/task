bddb0bf8fc1f569e42db4288bf9598bed004f292
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-11-14 15:05:14 +0530

    api response added

--- src/controllers/admin/admin.controller.js
+++ src/controllers/admin/admin.controller.js
@@ -1,11 +1,11 @@
 const User = require("../../models/user.model");
-
+const ApiResponse = require('../../utils/apiResponse');
 exports.createAdmin = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const user = new User({ name, email, password, role: "admin" });
     await user.save();
-    res.status(201).json(user);
+    res.status(201).json(new ApiResponse(200, "Admin created successfully.", user));
   } catch (error) {
     console.log(error);
   }
